a9d9a0f749cbb4f3f39fa464a74d08c3d716a189
zhangyemengren <624593758@qq.com>
2023-10-19 19:26:49 +0800

    feat: first invoke test

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import TitleBar from "./components/TitleBar.vue";
 </script>
 
 <template>
-    <div class="text-red-500">
-        header
+    <div>
+        <TitleBar/>
     </div>
 </template>
---
+++ src/components/TitleBar.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { invoke } from '@tauri-apps/api'
+async function callRust() {
+    const response = await invoke('test_cpu');
+    console.log(response);
+}
+</script>
+
+<template>
+    <div :class="['text-red-500', 'flex']">
+        <ul :class="['flex']">
+            <li @click="callRust">CPU</li>
+            <li>内存</li>
+            <li>能耗</li>
+            <li>磁盘</li>
+            <li>网路</li>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
