8aceaf88c5dbfe90c46375c93af83bbb2875dd12
zhangyemengren <624593758@qq.com>
2023-10-20 14:12:22 +0800

    feat: 部分ui

--- src/components/TitleBar.vue
+++ src/components/TitleBar.vue
@@ -1,20 +1,43 @@
-<script setup lang="ts">
-import { invoke } from '@tauri-apps/api'
+<script lang="ts" setup>
+import {invoke} from '@tauri-apps/api'
+import {ref} from "vue";
+
+const cpuInfo = ref('');
+
 async function callRust() {
-    const response = await invoke('test_cpu');
-    console.log(response);
+    cpuInfo.value = await invoke('test_cpu');
 }
 </script>
 
 <template>
-    <div :class="['text-red-500', 'flex']">
-        <ul :class="['flex']">
-            <li @click="callRust">CPU</li>
-            <li>内存</li>
-            <li>能耗</li>
-            <li>磁盘</li>
-            <li>网路</li>
-        </ul>
+    <div class="flex pl-[100px] bg-slate-700 h-[50px] items-center">
+        <div>
+            <p class="text-sm">活动监视器</p>
+            <p class="text-xs">所有进程</p>
+        </div>
+        <span class="isolate inline-flex rounded-md shadow-sm h-[30px]">
+            <button
+                class="relative inline-flex items-center rounded-l-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
+                type="button"
+                @click="callRust"
+            >CPU</button>
+            <button
+                class="relative -ml-px inline-flex items-center bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
+                type="button">内存</button>
+  <button
+      class="relative -ml-px inline-flex items-center bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
+      type="button">能耗</button>
+            <button
+                class="relative -ml-px inline-flex items-center bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
+                type="button">磁盘</button>
+  <button
+      class="relative -ml-px inline-flex items-center rounded-r-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
+      type="button">网络</button>
+
+</span>
+        <div>
+            {{ cpuInfo }}
+        </div>
     </div>
 </template>
 
